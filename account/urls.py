559a06fb369b8ce0f37af3f923853205258bc25a
Robert Ohuru <robertohuru@gmail.com>
2025-04-29 14:06:37 +0200

    Adding token refresh endpoint

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,14 @@
 # urls.py
 from django.urls import path
 from .views import LoginView, SignUpView, UserInfoView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('user-info/', UserInfoView.as_view(), name='user_info'),
 
